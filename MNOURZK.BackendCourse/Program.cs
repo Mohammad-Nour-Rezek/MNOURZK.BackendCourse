5f725b1c2fd3069eabb6d091f180efe00397c619
Mohammad Nour Rizq <Mohammed.Rizq@omicron-jo.com>
2024-11-18 05:53:29 +0300

    modify variables declarations and initialization

--- MNOURZK.BackendCourse/Program.cs
+++ MNOURZK.BackendCourse/Program.cs
@@ -4,32 +4,43 @@
     {
         static void Main(string[] args)
         {
+            double number1, number2, sum, min, mult, dev = default;
+            string? firstName, lastName, fullName = default;
+
             Console.WriteLine("\tCalculator App");
 
             Console.Write("Please enter the first number: ");
 
-            double number1 = Convert.ToDouble(Console.ReadLine());
+            number1 = Convert.ToDouble(Console.ReadLine());
 
             Console.Write("Please enter the second number: ");
 
-            double number2 = Convert.ToDouble(Console.ReadLine());
+            number2 = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine($"{number1} + {number2} = {number1 + number2}");
-            Console.WriteLine($"{number1} - {number2} = {number1 - number2}");
-            Console.WriteLine($"{number1} * {number2} = {number1 * number2}");
-            Console.WriteLine($"{number1} / {number2} = {number1 / number2}");
+            sum = number1 + number2;
+
+            min = number1 - number2;
+
+            mult = number1 * number2;
+
+            dev = number2 / number2;
+
+            Console.WriteLine($"{number1} + {number2} = {sum}");
+            Console.WriteLine($"{number1} - {number2} = {min}");
+            Console.WriteLine($"{number1} * {number2} = {mult}");
+            Console.WriteLine($"{number1} / {number2} = {dev}");
 
             Console.ReadKey();
 
             Console.Write("Please enter your first name: ");
 
-            string firstName = Console.ReadLine();
+            firstName = Console.ReadLine();
 
             Console.Write("Please enter your last name: ");
 
-            string lastName = Console.ReadLine();
+            lastName = Console.ReadLine();
 
-            string fullName = firstName + " " + lastName;
+            fullName = firstName + " " + lastName;
 
             Console.WriteLine(@$"Hello '{fullName}',
                                  Your full name length is {fullName.Length}");
